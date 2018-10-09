b0bb9afe37491547338cb787b3771b3d537642b9
Tran Vu Gia Vinh <vinh.tvg@ad.tctav.com>
2018-10-09 09:42:59 +0700

    Google login with PassportJS - 1

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('./passport');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var app = express();
+
+// social authentication
+require('./routes/auth')(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: '635819948834-q0aovduvu5p3m8j7lk0462mkcufjh3gc.apps.googleusercontent.com',
+    googleSecret: 'vsbXaQ2WCwp6l0ppZGeaSO1k'
+}
